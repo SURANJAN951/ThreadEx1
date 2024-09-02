48eb98931cce3d6a87469e787311be673de0d37a
Suranjan kumar <ksuranjan951@gmail.com>
2024-09-02 09:18:11 +0530

    Thread Example1

---
+++ ThreadEx1.java
@@ -0,0 +1,20 @@
+public class ThreadEx1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Thread");
+        System.out.println("Before changing");
+        String name =Thread.currentThread().getName();
+        System.out.println("The name of main thread is "+name);
+        System.out.println("The priority of main thread is "+ Thread.currentThread().getName());
+
+        System.out.println("After changing");
+        Thread t = Thread.currentThread();
+        t.setName("PW");
+        t.setPriority(1);
+
+        String name1=Thread.currentThread().getName();
+        System.out.println("The name of main thread is " + name1);
+        System.out.println("The priority of main thread is "+ Thread.currentThread().getName());
+    }
+}
